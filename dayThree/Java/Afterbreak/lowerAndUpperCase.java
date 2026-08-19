63dab7a47d805ff573900ebc38eed82ea40e6140
Rozarioehiz <rozarioehiz@gmail.com>
2026-08-19 17:15:16 +0100

    Day three after break

---
+++ dayThree/Java/Afterbreak/lowerAndUpperCase.java
@@ -0,0 +1,23 @@
+
+    public class LowerAndUpperCase{
+
+   
+      public static void main(String [] args){
+
+            String word = "a b c d e f ";
+
+            for(int  count = 0; count < word.length; count-- ){
+            
+               
+    
+    
+}
+
+
+
+
+      
+
+}
+
+}
